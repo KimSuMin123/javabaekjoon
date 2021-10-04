0a2a2735a860691061f9e8b6f3c23254fdc2fb0e
KimSuMin123 <86695347+KimSuMin123@users.noreply.github.com>
2021-10-04 11:05:03 +0900

    Add files via upload

---
+++ src/BronzeIV/korean.java
@@ -0,0 +1,15 @@
+package BronzeIV;
+
+import java.util.Scanner;
+
+public class korean {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = a + 44031;
+		
+		System.out.println((char)b);
+	}
+}
